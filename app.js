d904a0c2df3dedab8c1c96b1997e3262077fd948
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-07-06 22:46:52 -0300

    null

--- app.js
+++ app.js
@@ -37,3 +37,19 @@ const dateFormatter = new Intl.DateTimeFormat("pt-BR", {
 const date = new Date()
 
 const formattedDate = dateFormatter.format(date)
+
+tsParticles.load("tsparticles", {
+  particles: {
+    shape: {
+      type: "circle", // starting from v2, this require the square shape script
+    },
+    size: {
+      value: { min: 1, max: 4 },
+    },
+    move: {
+      enable: true,
+      speed: 0.1,
+    },
+  },
+  preset: "stars",
+})
